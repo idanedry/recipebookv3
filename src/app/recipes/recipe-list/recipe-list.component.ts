8846343a57ce5925ae826f63f90b06c79112f857
idanedry <idan.edry@gmail.com>
2017-04-01 13:02:43 +0300

    Added loading effect to recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,32 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipes.service';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import { AuthService } from '../../auth/auth.service';
+import { Subject} from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-  // recipes: Recipe[] = [];
-  // recipes: FirebaseListObservable<Recipe[]>;
-  recipes;	
-  private isLoading : boolean = false ;
+export class RecipeListComponent implements OnInit,OnDestroy {
+  private getRecipesSubscription : Subscription;
+  private loadingArraySubscription : Subscription;
+  private loadingArray = new Subject();
+  recipes;
+  private fakeRecipes = [];	
+  private isLoading : boolean = true ;
+  
   constructor( private _recipeService: RecipeService,
   			   private af: AngularFire,
            private _authService : AuthService) {}
 
   ngOnInit() {
-    // this.recipes = this._recipeService.getRecipes()
-    this.isLoading = true;
-  	this._recipeService.getRecipes()
+    for(let  i = 0; i < 4 ; i++) {
+      setTimeout(()=> { 
+        this.loadingArray.next(i)
+      },800 * i )
+    }
+
+    this.loadingArraySubscription = this.loadingArray.subscribe( res => {
+      this.fakeRecipes.push(res)
+    });
+
+  	this.getRecipesSubscription = this._recipeService.getRecipes()
       .subscribe( recipes => {
-                     this.recipes = recipes;
-                     this.isLoading = false;
-                   });
+         this.recipes = recipes;
+         this.isLoading = false;
+       });
   }
 
+  ngOnDestroy(){
+    this.getRecipesSubscription.unsubscribe();
+    this.loadingArraySubscription.unsubscribe();
+  }
 
 }
